43ed2866a4f5b189a03f7f33506fa902443ae84f
Roshan <19766713+rpalakkal@users.noreply.github.com>
2024-11-09 18:23:25 -0500

    feat: progress bar

--- src/bin/experiment.rs
+++ src/bin/experiment.rs
@@ -1,40 +1,53 @@
 use std::{fs::File, io::Write};
 
+use indicatif::{ProgressIterator, ProgressStyle};
 use path_oram::oram::PathORAM;
 
 const N: usize = 1 << 20;
 
+lazy_static::lazy_static! {
+    static ref STYLE: ProgressStyle = ProgressStyle::with_template(
+        "{spinner:.green} [{elapsed_precise}] [{bar:40.cyan/blue}] ({pos}/{len}, ETA {eta}) -- {msg}",
+    )
+    .unwrap();
+}
+
 fn experiment<const Z: usize>(warmup: usize, access: usize) {
+    println!("Running experiment with Z={}", Z);
+
     let mut oram = PathORAM::<u32, Z, N>::init().unwrap();
-    // write to the oram first
-    (0..N).for_each(|i| {
+
+    for i in (0..N)
+        .progress()
+        .with_style(STYLE.clone())
+        .with_message("Initial ORAM writes")
+    {
         oram.access(i, Some(i as u32)).unwrap();
-    });
+    }
 
-    log::info!("done intial write for z={}", Z);
-
-    // warmup
-    (0..warmup).for_each(|i| {
+    for i in (0..warmup)
+        .progress()
+        .with_style(STYLE.clone())
+        .with_message("ORAM warmup")
+    {
         oram.access(i % N, None).unwrap();
-    });
-
-    log::info!("done warmup for z={}", Z);
+    }
 
     let mut file = File::create(format!("data/{}.txt", Z)).unwrap();
     writeln!(file, "-1,{}", warmup).unwrap();
 
-    // measure stash size for each access
-    (0..access).for_each(|i| {
+    for i in (0..access)
+        .progress()
+        .with_style(STYLE.clone())
+        .with_message("Recording ORAM stash sizes")
+    {
         oram.access(i % N, None).unwrap();
         let stash_size = oram.stash.len();
         writeln!(file, "{},{}", i, stash_size).unwrap();
-    });
-
-    log::info!("done experiment for z={}", Z);
+    }
 }
 
 pub fn main() {
-    env_logger::init();
     experiment::<2>(1_000_000, 1_000_000);
     experiment::<4>(3_000_000, 10_000_000);
     experiment::<6>(3_000_000, 10_000_000);
